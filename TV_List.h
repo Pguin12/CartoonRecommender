b11316d7a426450078055854deeeabd118c90992
Pguin12 <74618829+Pguin12@users.noreply.github.com>
2025-04-22 12:35:31 -0400

    Update TV_List.h

--- TV_List.h
+++ TV_List.h
@@ -11,6 +11,34 @@ using namespace std;
 #ifndef TV_LIST_H
 #define TV_LIST_H
 
+/*
+
+class TV_List {
+    unordered_map<int, string> show_id;
+    unordered_map<int, string> type_id;
+    unordered_map<int, string> genre_id;
+    unordered_map<string, int> genretoid;
+    unordered_map<int, string> status_id;
+    unordered_map<string, unordered_set<int>> show_genre_list;
+
+    void set_show_genre_list();
+    void get_id(string my_file, unordered_map<int, string> &my_map);
+public:
+    TV_List();
+    unordered_map<string, unordered_set<int>>& get_show_genre_list()
+    {
+        return show_genre_list;
+    };
+    string get_genre(int my_id) {
+        return genre_id[my_id];
+    }
+    int get_genre_id(string name) {
+        return genretoid[name];
+    }
+};
+
+*/
+
 class TV_List {
     unordered_map<int, string> show_id;
     unordered_map<int, string> type_id;
